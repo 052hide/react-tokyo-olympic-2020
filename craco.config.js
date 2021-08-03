3a5a0e94adb2ef57582521f39b3f19e9f1924652
052hide <hidetoshi1990@gmail.com>
2021-08-03 12:07:04 +0900

    webpack設定

--- craco.config.js
+++ craco.config.js
@@ -4,4 +4,14 @@ module.exports = {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
+
+  webpack: {
+    configure: (webpackConfig, { env, paths }) => {
+      const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin') // eslint-disable-line @typescript-eslint/no-var-requires
+
+      webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(plugin => !(plugin instanceof ModuleScopePlugin));
+
+      return webpackConfig
+    }
+  },
 }
