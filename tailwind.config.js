ca74282f86588cd25b6ce1daa6d02685751fae81
052hide <hidetoshi1990@gmail.com>
2021-08-04 22:03:07 +0900

    画像表示までのガタつきを抑える

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
@@ -23,6 +26,10 @@ module.exports = {
         bronze: {
           default: '#934020',
         },
+      },
+      minHeight: {
+        '25vw': '25vw',
+        '50vw': '50vw',
       },
     },
   },
